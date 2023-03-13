7414e14366e109f553415e51bfce2f445c6e6e02
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2023-03-13 17:38:40 +0500

    Commit 2

--- next.config.js
+++ next.config.js
@@ -9,6 +9,6 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js' : undefined,
+  // assetPrefix: isProd ? 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js' : undefined,
 }
 
